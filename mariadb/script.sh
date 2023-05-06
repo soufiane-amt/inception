6e2679e77489f4a663ea27e737b62f57bf2aa9d3
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-06 19:49:58 +0000

    GOOD

--- mariadb/script.sh
+++ mariadb/script.sh
@@ -1,12 +1,11 @@
-db_name = MyDataBase
-db_user = DataBaseUser
-db_pwd = 1234
+MyDataBase = MyDataBase
+DataBaseUser = DataBaseUser
+PASSWORD = 1234
 
 
-echo "CREATE DATABASE $db_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "CREATE DATABASE $MyDataBase ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$DataBaseUser'@'%' IDENTIFIED BY '$PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MyDataBase.* TO '$DataBaseUser'@'%' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 service mysql start
