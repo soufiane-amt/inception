48eae5a928949f0714dc4d9c1a01a7642ea4972f
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-07 19:21:44 +0000

    boring

--- mariadb/script.sh
+++ mariadb/script.sh
@@ -4,14 +4,14 @@ MyDataBase="MyDataBase"
 DataBaseUser="DataBaseUser"
 PASSWORD="1234"
 
+service mysql start
 
 echo "CREATE DATABASE $MyDataBase ;" > db1.sql
 echo "CREATE USER  '$DataBaseUser'@'%' IDENTIFIED BY '$PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $MyDataBase.* TO '$DataBaseUser'@'%' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
-# service mysql start
-
-mysqld
 
 mysql < db1.sql
+
+/usr/bin/mysqld_safe
