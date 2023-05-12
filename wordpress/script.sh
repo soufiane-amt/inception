4b05e42c797f9fcbcb60a7bedb111ca47113d21d
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-12 17:03:03 +0000

    installing wordpress

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -13,4 +13,7 @@ wp cli update
 
 wp core download --allow-root
 
+mv wp-config-sample.php wp-config.php
+
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL  --allow-root
+
