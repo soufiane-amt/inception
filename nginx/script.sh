0cd5b44bf050ea52cba8011d8f2070b0026fa2e0
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-13 19:42:07 +0000

    ngi

--- nginx/script.sh
+++ nginx/script.sh
@@ -1,5 +1,11 @@
-openssl genrsa -des3 -passout pass: -out server.key 2048
+#!/bin/bash
 
-openssl req -x509 -new -nodes -key server.key -sha256 -days 1825 -out server.crt -config openssl.conf -batch
+cd /home/www/openssl
+
+openssl genrsa -des3 -passout pass:"0000" -out server.key 2048
+
+openssl req -x509 -new -nodes -key server.key -sha256 -days 1825 -out server.crt -config openssl.conf -batch -passin pass:"0000"
+
+service nginx restart
 
 nginx -g "daemon off"
