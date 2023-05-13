47d1bc68ff1c832c24f9e5a2fb78eec80beeffd9
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-13 18:46:16 +0000

    testing nginx

--- nginx/script.sh
+++ nginx/script.sh
@@ -1,3 +1,5 @@
 openssl genrsa -des3 -passout pass: -out server.key 2048
 
 openssl req -x509 -new -nodes -key server.key -sha256 -days 1825 -out server.crt -config openssl.conf -batch
+
+nginx -g "daemon off"
