5374732423b71c318b49bbed7bc26fdd9524dab5
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-13 17:36:54 +0000

    dockef

--- nginx/script.sh
+++ nginx/script.sh
@@ -1,3 +1,3 @@
-openssl genrsa -des3 -out myCA.key 2048
+openssl genrsa -des3 -out server.key 2048
 
-openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem -config openssl.conf -batch
+openssl req -x509 -new -nodes -key server.key -sha256 -days 1825 -out server.crt -config openssl.conf -batch
