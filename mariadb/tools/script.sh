e7cc250cb4dc62f6bd14d769cd4460565f19aeb0
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-17 19:20:16 +0000

    even maria-db works fine

--- mariadb/tools/script.sh
+++ mariadb/tools/script.sh
@@ -8,6 +8,4 @@ mysql -u root  -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_P
 mysql -u root  -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" ;
 mysql -u root  -e "FLUSH PRIVILEGES;"
 
-kill `cat /var/run/mysqld/mysqld.pid`
-
 mysqld
