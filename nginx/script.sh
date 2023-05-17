4b7751ec1d632b1022f70612bab6b826ace87b51
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-17 17:36:50 +0000

    nginx is ready, maria-db still got problem it

--- nginx/script.sh
+++ nginx/script.sh
@@ -6,5 +6,4 @@ openssl genrsa  -out /home/www/openssl/server.key
 
 openssl req -x509 -new -key /home/www/openssl/server.key -sha256 -days 356 -out /home/www/openssl/server.crt -config /home/www/openssl/openssl.conf 
 
-service nginx start
- 
+nginx -g 'daemon off;'
