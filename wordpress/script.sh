841139f4ba3d70920ffa079602b0d6e2c072fbb7
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-18 20:14:27 +0000

    finished

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-mkdir wordpress
-
-cd wordpress
+chmod +x /var/www/html/index.php
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
@@ -29,6 +27,8 @@ sed -i "s/localhost/mariadb/1" wp-config.php
 cat wp-config.php
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL  --allow-root
+
+wp user create $WP_USR $WP_USR_EMAIL --role=author --user_pass=$WP_USR_PWD --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
