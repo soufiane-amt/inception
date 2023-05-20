cd7d5e27291142f58f915c2e38f6f7e715710e11
Soufiane Amajat <soufianeamt11@gmail.com>
2023-05-20 14:03:13 +0000

    good

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,11 +4,12 @@ sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
-mysql -u root  -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
-mysql -u root  -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'" ;
-mysql -u root  -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" ;
-mysql -u root  -e "FLUSH PRIVILEGES;"
 
+mysql -u root -p$DB_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
+mysql -u root -p$DB_PASS -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'" ;
+mysql -u root -p$DB_PASS -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" ;
+mysql -u root -p$DB_PASS -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" 
+mysql -u root -p$DB_PASS -e "FLUSH PRIVILEGES;"
 
 kill `cat /var/run/mysqld/mysqld.pid`
 
